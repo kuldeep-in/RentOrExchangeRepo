50fbc24bea7726a5c841fb72c9ab55c02071a5cb
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-10-17 10:16:00 +0100

    Login and Register functionality

--- RentOrExchange.WebApp/Areas/Identity/Data/MyAppUser.cs
+++ RentOrExchange.WebApp/Areas/Identity/Data/MyAppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,5 +10,12 @@ namespace RentOrExchange.WebApp.Areas.Identity.Data
     // Add profile data for application users by adding properties to the MyAppUser class
     public class MyAppUser : IdentityUser
     {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
     }
 }
