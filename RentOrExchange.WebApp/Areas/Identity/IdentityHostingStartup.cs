c94bcd6458288d55be05f4bce650525cf7f986db
Kuldeep Singh <in.singhkuldeep@gmail.com>
2021-10-17 08:57:20 +0100

    New project created

---
+++ RentOrExchange.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentOrExchange.WebApp.Areas.Identity.Data;
+using RentOrExchange.WebApp.Data;
+
+[assembly: HostingStartup(typeof(RentOrExchange.WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace RentOrExchange.WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DBContextConnection")));
+
+                services.AddIdentity<MyAppUser, IdentityRole>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<DBContext>();
+            });
+        }
+    }
+}
